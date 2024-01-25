c1ec42e963db3284a3629ed6e93422298d6d6a6a
ZhangXin <zhang-xin@hotmail.com>
2024-01-25 09:23:19 +0200

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
