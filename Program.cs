0e33b1a5a705cce0f192a0d0773a641180ea51a9
ZhangXin <zhang-xin@hotmail.com>
2024-03-20 20:58:18 +0200

    1

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ namespace Management_System
         public static string user_name;
         static void Main()
         {
-            /*
+            
             IDatabaseOperations dbOperations = new MySqlDatabaseOperations();
             DatabaseSerializerClient client = new DatabaseSerializerClient(dbOperations);
             client.SerializeDatabaseToFile("D:\\DatabaseSnapshot.json", "1.0");
-            */
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
